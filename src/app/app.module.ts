b5cd6d18d9910c92c0413ae98a67d80deff66da3
James Yin <jyin88@gmail.com>
2019-09-22 20:11:30 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  MatCardModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTableModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { DocListComponent } from './doc-list/doc-list.component';
+import { DocDetailComponent } from './doc-detail/doc-detail.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'docs/:id',
+    component: DocDetailComponent
+  },
+  {
+    path: 'docs',
+    component: DocListComponent,
+    data: { title: 'Inh Docs List' }
+  },
+  { path: '', redirectTo: '/docs', pathMatch: 'full' },
+  { path: '**', redirectTo: '/docs' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DocListComponent, DocDetailComponent],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/doc-detail/doc-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-detail',
+  templateUrl: './doc-detail.component.html'
+})
+export class DocDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
